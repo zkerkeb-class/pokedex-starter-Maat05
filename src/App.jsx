ffde97dd2e1b4013af68214cc819e2941cea0adb
martin <martinmullerparis@gmail.com>
2025-02-13 13:57:58 +0100

    shiny

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,37 @@ import PokemonCard from './components/pokemonCard'
 import './App.css'
 
 function App() { 
-  const [filter, setFilter] = useState("");
+  const [filterNom, setFilterNom] = useState("");
+  const [filterType, setFilterType] = useState("");
 
   const filteredPokemons = pokemons.filter((pokemon) =>
-    pokemon.name.french.toLowerCase().includes(filter.toLowerCase())
+    pokemon.name.french.toLowerCase().includes(filterNom.toLowerCase())
+  );
+
+  const filteredTypes = filteredPokemons.filter((pokemon) => 
+    pokemon.type.some((type) => type.toLowerCase().includes(filterType.toLowerCase()))
   );
 
   return ( 
     <div className="app"> 
       <h1>Liste des Pokémons</h1> 
+      
       <input 
         type="text" 
         placeholder="Filtrer par nom..." 
-        value={filter} 
-        onChange={(e) => setFilter(e.target.value)} 
-      /> 
+        value={filterNom} 
+        onChange={(e) => setFilterNom(e.target.value)} 
+      />
+      
+      <input 
+        type="text" 
+        placeholder="Filtrer par type..." 
+        value={filterType} 
+        onChange={(e) => setFilterType(e.target.value)} 
+      />
+
       <div className="pokemon-list"> 
-        {filteredPokemons.map((pokemon, index) => ( 
+        {filteredTypes.map((pokemon, index) => ( 
           <PokemonCard 
             key={index} 
             name={pokemon.name.french} 
