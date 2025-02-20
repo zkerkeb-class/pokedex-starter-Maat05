7bbd5de36a16dc3b24ccb0f1d1371498500069ea
martin <martinmullerparis@gmail.com>
2025-02-20 11:14:30 +0100

    shiny

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,51 @@ import PokemonCard from './components/pokemonCard'
 import SearchBar from './components/searchBar'
 import './App.css'
 
+function App() { const [search, setSearch] = useState("")
+const [types, setTypes] = useState([])
+const [shinyStates, setShinyStates] = useState({})
 
-function App() {
-  const [search, setSearch] = useState("")
-  const [types, setTypes] = useState([])
+const toggleShiny = (id) => { setShinyStates((prev) => ({ ...prev, [id]: !prev[id] })) }
 
-  useEffect(() => {
-    console.log(search)
-    console.log('types', types)
-  }, [search, types])
+useEffect(() => { console.log(search)
+console.log('types', types) }, [search, types])
 
-  return (
-    <div className="app-container">
-      <SearchBar types={types} setTypes={setTypes} search={search} setSearch={setSearch}/>
+return ( <div className="app-container"> <SearchBar types={types} setTypes={setTypes} search={search} setSearch={setSearch} />
 
-    <div className="pokemon-list">
-      {pokemons.map((pokemon) => {
-        const isTypeIncluded = types.length === 0 || types.every(type => pokemon.type.includes(type))
-        const isNameIncluded = search === "" || pokemon.name.french.toLowerCase().includes(search.toLowerCase())
+  <div className="pokemon-list">
+    {pokemons.map((pokemon) => {
+      const isTypeIncluded =
+        types.length === 0 || types.every((type) => pokemon.type.includes(type))
+      const isNameIncluded =
+        search === "" || pokemon.name.french.toLowerCase().includes(search.toLowerCase())
 
-        if(!isNameIncluded || !isTypeIncluded){
-          return null
-        }
-        
-        return (
-          <div key={pokemon.id} className="pokemon-card-container">
-          <PokemonCard 
-          name={pokemon.name.french} 
-          types={pokemon.type} 
-          image={pokemon.imageShiny}
-          attack={pokemon.base.Attack}
-          defense={pokemon.base.Defense}
-          hp={pokemon.base.HP}
-        />
+      if (!isNameIncluded || !isTypeIncluded) {
+        return null
+      }
+
+      const isShiny = shinyStates[pokemon.id] || false
+      const currentImage = isShiny ? pokemon.imageShiny : pokemon.image
+
+      return (
+        <div
+          key={pokemon.id}
+          className="pokemon-card-container"
+          onClick={() => toggleShiny(pokemon.id)}
+        >
+          <PokemonCard
+            name={pokemon.name.french}
+            types={pokemon.type}
+            image={currentImage}
+            attack={pokemon.base.Attack}
+            defense={pokemon.base.Defense}
+            hp={pokemon.base.HP}
+          />
         </div>
-        )
-      })}
-    </div>
-    </div>
-  )
-}
+      )
+    })}
+  </div>
+</div>
+
+) }
 
 export default App
